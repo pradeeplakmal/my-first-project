1bb4496a0376e36a20911e6890af137b27be6211
pradeeplakmal <pradeeplakmal1982@gmail.com>
2024-12-09 16:26:57 +0530

    Update Login Page

---
+++ app/login/page.jsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+    return(
+        <div className="flex flex-col items-center justify-center w-full min-h-screen text-center bg-slate-300">
+            <div className="px-5 py-3 bg-red-500 rounded-lg">Login Page</div>
+        </div>
+    )
+}
