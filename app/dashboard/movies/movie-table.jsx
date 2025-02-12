638ad7db2575d1d8fd0e1ec312db8cb4900e2616
pradeeplakmal <pradeeplakmal1982@gmail.com>
2025-02-12 09:36:30 +0530

    Update Movie Table First Step

--- app/dashboard/movies/movie-table.jsx
+++ app/dashboard/movies/movie-table.jsx
@@ -1,6 +1,45 @@
 "use client";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default function MovieTable({ movies }) {
   console.log("Movies Table Client", movies);
-  return <div>Movie Table 01</div>;
+
+  return (
+    <div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="font-bold"># Cover</TableHead>
+            <TableHead className="font-bold">Movie Title</TableHead>
+            <TableHead className="font-bold">Year</TableHead>
+            <TableHead className="font-bold">Rated</TableHead>
+            <TableHead className="font-bold">IMDb</TableHead>
+            <TableHead className="font-bold">Genres</TableHead>
+            <TableHead className="font-bold">Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {movies.map((movie) => (
+            <TableRow key={movie.id}>
+              <TableCell>Poster URL</TableCell>
+              <TableCell>{movie?.title ?? "N/A"}</TableCell>
+              <TableCell>{movie?.year ?? "N/A"}</TableCell>
+              <TableCell>{movie?.rated ?? "N/A"}</TableCell>
+              <TableCell>{movie?.imdb?.rating ?? 0}</TableCell>
+              <TableCell>{movie?.genres?.join(", ")}</TableCell>
+              <TableCell>Actions</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
